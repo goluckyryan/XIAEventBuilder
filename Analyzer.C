06d3e5260186e1ecd7a8454e1830d085312babba
Ryan@Ubuntu <rtang@fsu.edu>
2021-12-14 11:14:58 -0500

    edit drawE()

--- Analyzer.C
+++ Analyzer.C
@@ -146,17 +146,36 @@ void rawID(){
 
 void drawE(bool isLogy = false, bool cali = false){
 
+   int numCol = NCLOVER / 4;
+
    TCanvas *cRawE = (TCanvas *) gROOT->FindObjectAny("cRawE");
-   if( cRawE == NULL ) cRawE = new TCanvas("cRawE", "raw e", 800, 1500);
-   cRawE->Clear();cRawE->Divide(4,9);
-   for (Int_t i = 0; i < 36; i++) {
-      cRawE->cd(i+1); 
-      cRawE->cd(i+1)->SetGrid();
-      if( isLogy ) cRawE->cd(i+1)->SetLogy();
-      if( cali ) {
-         heCal[i]->Draw("");
-      }else{
-         he[i]->Draw("");
+   if( cRawE == NULL ) cRawE = new TCanvas("cRawE", cali ? "Cal e" : "Raw e", 1200, 800);
+   cRawE->Clear();cRawE->Divide(numCol, 4);
+   
+   //cRawE->SetRightMargin(0);
+   //cRawE->SetLeftMargin(0);
+   //cRawE->SetTopMargin(0);
+   //cRawE->SetBottomMargin(0);
+   //cRawE->SetTicks(1,1);
+   //cRawE->SetBorderMode(1);
+   
+   for (Int_t i = 0; i < 4; i++) {
+      for( Int_t j = 0; j < numCol; j++){
+         int canvasID = numCol * i + j + 1;
+         cRawE->cd(canvasID); 
+         cRawE->cd(canvasID)->SetGrid();       
+         cRawE->cd(canvasID)->SetRightMargin(0.1);
+         //cRawE->cd(canvasID)->SetLeftMargin(0);
+         cRawE->cd(canvasID)->SetTopMargin(0);
+         //cRawE->cd(canvasID)->SetBottomMargin(0);  
+         //cRawE->cd(canvasID)->SetBorderMode(1);  
+         if( isLogy ) cRawE->cd(canvasID)->SetLogy();
+         int hID = 4*j+ i;
+         if( cali ) {
+            heCal[hID]->Draw("");
+         }else{
+            he[hID]->Draw("");
+         }
       }
    }
 
